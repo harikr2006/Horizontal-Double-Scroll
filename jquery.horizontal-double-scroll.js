74d4dda7c221a62bd541f1456e2600837dcb46b9
Harikrishnan D (harikr2006) <harikr2006@gmail.com>
2016-04-07 16:19:47 +0530

    window resize
    while window resize automatically update the scroll

--- jquery.horizontal-double-scroll.js
+++ jquery.horizontal-double-scroll.js
@@ -1,19 +1,56 @@
-(function($){
-            $.fn.hds = function(options){
-                this.each(function(){
-                    $(this).css({"overflow":"scroll"});
-					
-					  $(this).before("<div class='tscroll'><div class='tscroll-inner'></div></div>").parent().find("div.tscroll").width($(this).width()).css({"height":"20px","overflow":"scroll"}).find("div.tscroll-inner").css({"width":$(this).find('>:first-child').width(),"height":"20px"});
-					  $(this).prev().scrollLeft($(this).scrollLeft());
-					  $(this).on("scroll",function(){
-						  $(this).prev().scrollLeft($(this).scrollLeft());
-						  });
-						  
-						  $(this).prev().on("scroll",function(){
-						  $(this).next().scrollLeft($(this).scrollLeft());
-						  });
-					
-             });
-            }
-            
-        })(jQuery)
+(function($) {
+    'use strict'
+    $.fn.hds = function(options) {
+        var opts = $.extend({}, $.fn.hds.defaults, options);
+        this.each(function() {
+            var _this = $(this);
+            $.fn.template(_this, opts);
+            $(window).resize(function() {
+                $.fn.template(_this, opts);
+            })
+        });
+
+    };
+
+
+    $.fn.hds.defaults = {
+        style: "overflow:auto",
+        scrollHeight: "15",
+        scrollBufferWidth: "15",
+        parent_class:"tscroll",
+        child_class:"tscroll-inner",
+        template: "<div class='tscroll'><div class='tscroll-inner'></div></div>"
+    };
+
+    $.fn.template = function(ele, opts) {
+        
+        if (!$("."+opts.parent_class).length) {
+            ele.attr("style", opts.style)
+                .before(opts.template);
+        }
+        $.fn.setWidth(ele, opts);
+    }
+
+    $.fn.setWidth = function(ele, opts) {
+        var _secondary_scroll_parent = ele.prev();
+        var _secondary_scroll_child = $("."+opts.child_class, _secondary_scroll_parent);
+        var _this_width = ele.width();
+        var _this_child_width = $("> :first-child", ele).outerWidth();
+
+        _secondary_scroll_parent.attr("style",opts.style)
+        _secondary_scroll_parent.width(_this_width);
+        _secondary_scroll_child.width(_this_child_width).height(opts.scrollHeight - (opts.scrollHeight-1));
+
+        $.fn.updateBoth(ele, opts, _secondary_scroll_parent);
+    }
+
+    $.fn.updateBoth = function(ele, opts, parent) {
+        parent.on("scroll", function() {
+            ele.scrollLeft($(this).scrollLeft());
+
+        })
+        ele.on("scroll", function() {
+            parent.scrollLeft($(this).scrollLeft());
+        })
+    }
+}(jQuery));
